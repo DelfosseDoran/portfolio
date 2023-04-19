738fe2d16fb86e1e722c7bb39bf9ce3e12588729
DelfosseDoran <doran.delfosse@student.howest.be>
2023-04-20 00:33:04 +0200

    wright size art

--- portfolio/src/components/Art.tsx
+++ portfolio/src/components/Art.tsx
@@ -6,28 +6,27 @@ export default ({
   linklg,
   linkxl,
   alt,
-  start,
-  col,
-  row,
+  divClass,
+  media = '(max-width: 400px) 300px, (max-width: 639px) 600px,(max-width:1024px) 200px,(max-width:1536px) 250px,500px',
 }: {
   link: string;
   linkxs?: string;
   linksm?: string;
-  linkm?: string
+  linkm?: string;
   linklg?: string;
   linkxl?: string;
   alt: string;
-  start?: string;
-  col?: string;
-  row?: string;
+  divClass?: string;
+  media?: string;
 }) => {
   return (
-    <div className={` p-4 bg-royal-purple ${col} ${row} ${start} `}>
+    <div className={` p-4 bg-royal-purple ${divClass} `}>
       <img
-        className=" justify-center items-center w-full h-full bg-white"
+        className=" h-fit w-full justify-center items-center bg-white"
         src={link}
         srcSet={`${linkxs} 300w, ${linksm} 400w, ${linkm} 500w, ${linklg} 700w, ${linkxl} 900w`}
-        sizes="(max-width: 400px) 300px, (max-width: 639px) 600px,(max-width:1024px) 200px,(max-width:1536px) 250px,500px"
+        sizes={media}
+        alt={alt}
       />
     </div>
   );
--- portfolio/src/routes/DigitalArt.tsx
+++ portfolio/src/routes/DigitalArt.tsx
@@ -14,12 +14,11 @@ export default () => {
           link="https://res.cloudinary.com/dckotgjju/image/upload/v1681928725/blauw-rood-druk_nbirx7.jpg"
           linkxl="https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_900,c_scale/blauw-rood-druk_nbirx7.jpg"
           linklg="https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_700,c_scale/blauw-rood-druk_nbirx7.jpg"
-          linkm ="https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_500,c_scale/blauw-rood-druk_nbirx7.jpg"
+          linkm="https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_500,c_scale/blauw-rood-druk_nbirx7.jpg"
           linksm="https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_400,c_scale/blauw-rood-druk_nbirx7.jpg"
           linkxs="https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_300,c_scale/blauw-rood-druk_nbirx7.jpg"
           alt="Digital Art"
-          start="lg:col-start-1 row-start-2"
-          row="row-span-3"
+          divClass="lg:col-start-1 row-start-2 row-span-3"
         />
         <Art
           link="https://res.cloudinary.com/dckotgjju/image/upload/blokken_qindmy.jpg"
@@ -29,8 +28,7 @@ export default () => {
           linksm="https://res.cloudinary.com/dckotgjju/image/upload/w_400,c_scale/blokken_qindmy.jpg"
           linkxs="https://res.cloudinary.com/dckotgjju/image/upload/w_300,c_scale/blokken_qindmy.jpg"
           alt="Digital Art"
-          start="lg:col-start-3 lg:row-start-1 col-start-2 row-start-3"
-          row="row-span-3"
+          divClass="lg:col-start-3 lg:row-start-1 col-start-2 row-start-3 row-span-3"
         />
         <Art
           link="https://res.cloudinary.com/dckotgjju/image/upload/clos-up-dier_ma2fzr.jpg"
@@ -40,8 +38,7 @@ export default () => {
           linksm="https://res.cloudinary.com/dckotgjju/image/upload/w_400,c_scale/clos-up-dier_ma2fzr.jpg"
           linkxs="https://res.cloudinary.com/dckotgjju/image/upload/w_300,c_scale/clos-up-dier_ma2fzr.jpg"
           alt="Digital Art"
-          start="lg:col-start-4  row-start-1"
-          row="row-span-3"
+          divClass="lg:col-start-4  row-start-1 row-span-3"
         />
         <Art
           link="https://res.cloudinary.com/dckotgjju/image/upload/fooroskoop_wbhvuf.jpg"
@@ -51,8 +48,7 @@ export default () => {
           linksm="https://res.cloudinary.com/dckotgjju/image/upload/w_400,c_scale/fooroskoop_wbhvuf.jpg"
           linkxs="https://res.cloudinary.com/dckotgjju/image/upload/w_300,c_scale/fooroskoop_wbhvuf.jpg"
           alt="Digital Art"
-          start="lg:col-start-5 lg:row-start-3"
-          row="row-span-3"
+          divClass="lg:col-start-5 lg:row-start-3 row-span-3"
         />
         <Art
           link="https://res.cloudinary.com/dckotgjju/image/upload/getal_ipvya4.jpg"
@@ -62,9 +58,8 @@ export default () => {
           linksm="https://res.cloudinary.com/dckotgjju/image/upload/w_400,c_scale/getal_ipvya4.jpg"
           linkxs="https://res.cloudinary.com/dckotgjju/image/upload/w_300,c_scale/getal_ipvya4.jpg"
           alt="Digital Art"
-          start="1"
-          col="lg:col-span-2"
-          row="lg:row-span-4 row-span-2"
+          divClass="lg:col-span-2 lg:row-span-4 row-span-2"
+          media="(max-width: 400px) 300px, (max-width: 639px) 600px,(max-width:1024px) 200px,(max-width:1536px) 450px,800px"
         />
         <Art
           link="https://res.cloudinary.com/dckotgjju/image/upload/stuk-groentefruit_yxw1as.jpg"
@@ -74,19 +69,18 @@ export default () => {
           linksm="https://res.cloudinary.com/dckotgjju/image/upload/w_400,c_scale/stuk-groentefruit_yxw1as.jpg"
           linkxs="https://res.cloudinary.com/dckotgjju/image/upload/w_300,c_scale/stuk-groentefruit_yxw1as.jpg"
           alt="lg:Digital Art"
-          row="row-span-2"
+          divClass="row-span-2"
         />
 
         <Art
           link="https://res.cloudinary.com/dckotgjju/image/upload/portret_mro6gt.png"
           linkxl="https://res.cloudinary.com/dckotgjju/image/upload/w_900,c_scale/portret_mro6gt.png"
           linklg="https://res.cloudinary.com/dckotgjju/image/upload/w_700,c_scale/portret_mro6gt.png"
-          linkm ="https://res.cloudinary.com/dckotgjju/image/upload/w_500,c_scale/portret_mro6gt.png"
+          linkm="https://res.cloudinary.com/dckotgjju/image/upload/w_500,c_scale/portret_mro6gt.png"
           linksm="https://res.cloudinary.com/dckotgjju/image/upload/w_400,c_scale/portret_mro6gt.png"
           linkxs="https://res.cloudinary.com/dckotgjju/image/upload/w_300,c_scale/portret_mro6gt.png"
           alt="Digital Art"
-          start="1"
-          row="row-span-3"
+          divClass="row-span-3"
         />
         <Art
           link="https://res.cloudinary.com/dckotgjju/image/upload/klonen_onhzjg.jpg"
@@ -96,9 +90,8 @@ export default () => {
           linksm="https://res.cloudinary.com/dckotgjju/image/upload/w_400,c_scale/klonen_onhzjg.jpg"
           linkxs="https://res.cloudinary.com/dckotgjju/image/upload/w_300,c_scale/klonen_onhzjg.jpg"
           alt="Digital Art"
-          start="1"
-          row="lg:row-span-4 row-span-3"
-          col="lg:col-span-2"
+          divClass="lg:row-span-4 row-span-3 lg:col-span-2"
+          media="(max-width: 400px) 300px, (max-width: 639px) 600px,(max-width:1024px) 200px,(max-width:1536px) 450px,800px"
         />
         <div className="lg:hidden row-span-2 bg-royal-purple"></div>
         <Art
@@ -109,8 +102,8 @@ export default () => {
           linksm="https://res.cloudinary.com/dckotgjju/image/upload/w_400,c_scale/space_zmmzyx.jpg"
           linkxs="https://res.cloudinary.com/dckotgjju/image/upload/w_300,c_scale/space_zmmzyx.jpg"
           alt="Digital Art"
-          start="1"
-          col="col-span-2"
+          divClass="col-span-2"
+          media="(max-width: 400px) 300px, (max-width: 639px) 600px,(max-width:1024px) 450px,(max-width:1536px) 450px,800px"
         />
         <Art
           link="https://res.cloudinary.com/dckotgjju/image/upload/transpatuur_plqbyg.jpg"
@@ -120,7 +113,6 @@ export default () => {
           linksm="https://res.cloudinary.com/dckotgjju/image/upload/w_400,c_scale/transpatuur_plqbyg.jpg"
           linkxs="https://res.cloudinary.com/dckotgjju/image/upload/w_300,c_scale/transpatuur_plqbyg.jpg"
           alt="Digital Art"
-          start="1"
           // row="row-span-3"
         />
         <div className=" col-span-3 bg-royal-purple"></div>
