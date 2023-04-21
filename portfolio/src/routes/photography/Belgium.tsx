243dc192da14eb20f2f0281a50e0c6cf5f01b11d
DelfosseDoran <doran.delfosse@student.howest.be>
2023-04-21 20:33:46 +0200

    media belguim

--- portfolio/src/routes/photography/Belgium.tsx
+++ portfolio/src/routes/photography/Belgium.tsx
@@ -13,54 +13,72 @@ export default () => {
           alt="Chiken"
           divClass="lg:col-span-2 row-span-3 lg:col-start-1 lg:row-start-1 row-start-2"
           color="bg-hunyadi-yelow"
+          media3={0}
+          media5={1}
         />
         <Image
           link="Belgie/DSC_0005_2_lbmczi.jpg"
           alt="Chiken"
           divClass="lg:row-span-1 lg:col-start-3 lg:row-start-3 lg:row-span-1 row-span-3 col-start-2"
           color="bg-hunyadi-yelow"
+          media3={0}
+          media5={0}
         />
         <Image
           link="Belgie/DSC_0765_blalyp.jpg"
           alt="Chiken"
           divClass="row-span-3 lg:row-start-2 lg:col-span-2 lg:col-start-4 row-start-3 col-start-3"
           color="bg-hunyadi-yelow"
+          media3={0}
+          media5={1}
         />
         <Image
           link="Belgie/DSC_0741_qabqen.jpg"
           alt="Chiken"
           divClass="lg:row-span-2 lg:row-start-4 col-span-2 row-span-3 lg:col-span-1"
           color="bg-hunyadi-yelow"
+          media3={1}
+          media5={0}
         />
         <Image
           link="Belgie/DSC_0012_w5mvt8.jpg"
           alt="Chiken"
           divClass="lg:row-span-3 lg:col-span-2 "
           color="bg-hunyadi-yelow"
+          media3={0}
+          media5={1}
         />
         <Image
           link="Belgie/DSC_0008_dlo9si.jpg"
           alt="Chiken"
           divClass="lg:row-span-6 lg:col-span-2 row-span-3 "
           color="bg-hunyadi-yelow"
+          media3={0}
+          media5={1}
         />
         <Image
           link="Belgie/DSC_0053_otjhcm.jpg"
           alt="Chiken"
           divClass="lg:row-span-1 "
           color="bg-hunyadi-yelow"
+          media3={0}
+          media5={0}
         />
         <Image
           link="Belgie/DSC_0147_f7t97w.jpg"
           alt="Chiken"
           divClass="lg:row-span-6 lg:col-span-3"
           color="bg-hunyadi-yelow"
+          media3={0}
+          media5={2}
         />
         <Image
           link="Belgie/DSC_0006_heuzfm.jpg"
           alt="Chiken"
           divClass="row-span-2 lg:col-span-1 col-span-2"
           color="bg-hunyadi-yelow"
+          media3={1}
+          media5={0}
         />
 
         <Image
@@ -68,6 +86,8 @@ export default () => {
           alt="Chiken"
           divClass="lg:row-span-2 "
           color="bg-hunyadi-yelow"
+          media3={0}
+          media5={0}
         />
       </div>
     </>
