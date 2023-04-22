f0acb9232b2100ab7633602ed98e2d81b4130227
DelfosseDoran <doran.delfosse@student.howest.be>
2023-04-22 19:55:45 +0200

    projects

--- portfolio/src/components/Project.tsx
+++ portfolio/src/components/Project.tsx
@@ -1,4 +1,5 @@
 import { ChevronDown } from 'lucide-react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default ({
@@ -9,6 +10,7 @@ export default ({
   reverse,
   link,
   start,
+  alt
 }: {
   reverse?: string;
   srcLink: string;
@@ -17,7 +19,9 @@ export default ({
   listTegnologys: string[];
   link?: string;
   start: string;
+  alt: string;
 }) => {
+  const [show, setShow] = useState(false);
   let list;
   list = listTegnologys.map((item, index) => {
     return <li key={index}>{item}</li>;
@@ -26,11 +30,24 @@ export default ({
     <div className={` col-span-11 lg:flex gap-4 pb-6 ${start} ${reverse}`}>
       {/* flex */}
       <div className="lg:w-2/5">
-        <img src={srcLink} alt={`foto of ${projectName}`} />
+        <img
+          className=" h-fit w-full justify-center items-center bg-white"
+          src={`https://res.cloudinary.com/dckotgjju/image/upload/${srcLink}`}
+          srcSet={`
+        https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_300,c_scale/${srcLink} 300w, 
+        https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_350,c_scale/${srcLink} 350w, 
+        https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_450,c_scale/${srcLink} 450w, 
+        https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_550,c_scale/${srcLink} 550w, 
+        https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_600,c_scale/${srcLink} 600w, 
+        https://res.cloudinary.com/dckotgjju/image/upload/f_auto,w_750,c_scale/${srcLink} 750w, 
+        `}
+          sizes="(max-width: 400px) 350px, (max-width: 639px) 550px, (max-width: 1024px) 750px, (max-width: 1536px) 450px, 600px"
+          alt={alt}
+        />
       </div>
-      <div className=" lg:w-3/5 bg-tea-green flex flex-col justify-between p-2">
+      <div className=" lg:w-3/5 bg-tea-green flex flex-col justify-between p-2 h-fit">
         <h1 className="mb-4 text-2xl font-medium">{projectName}</h1>
-        <p className="mb-2">{projectText}</p>
+        <p className="mb-2 xl:text-xl">{projectText}</p>
         <div>
           {link ? (
             <div className="my-3">
@@ -44,12 +61,15 @@ export default ({
           ) : null}
           <div className=" bg-Feldgrau p-2 text-lemon-chiffon">
             <button
-              className={`peer focus:pb-2 w-full text-lg font-medium text-left flex justify-between`}
+              className={`focus:pb-2 w-full text-lg font-medium text-left flex justify-between`}
+              onClick={() => {
+                setShow(!show);
+              }}
             >
               <p>Technologies</p>
               <ChevronDown />
             </button>
-            <ul className={` peer-focus:block ml-2 hidden`}>{list}</ul>
+            <ul className={` ${show ? 'block' : 'hidden'} ml-2 `}>{list}</ul>
           </div>
         </div>
       </div>
--- portfolio/src/routes/Projects.tsx
+++ portfolio/src/routes/Projects.tsx
@@ -8,16 +8,18 @@ export default () => {
         projectText="For Frontend Development class, we had to make a portfolio. So now
               you can see how I managed."
         listTegnologys={['React', 'Typescript', 'Tailwind', 'React Router']}
-        srcLink=""
+        srcLink="projects/55shots_so_od46ig.png"
         start="col-start-1"
+        alt="foto of portfolio"
       />
       <Project
         projectName="Car Sharing App"
         projectText="This is an app for people who share a car. So they can see who has driven where. You can also see how much you have to pay the owner."
         listTegnologys={['React Native', 'Expo']}
-        srcLink=""
+        srcLink="projects/218shots_so_qzudqb.png"
         start="col-start-2"
         reverse="flex-row-reverse"
+        alt="foto of car sharing app"
       />
       <Project
         projectName="Interactive Poles"
@@ -33,23 +35,26 @@ export default () => {
         ]}
         srcLink=""
         start="col-start-1"
+        alt="foto of interactive poles"
       />
       <Project
         projectName="Europe Population"
         projectText="A website that I had to make for the Interaction Design course. I made a website where you can compare countries geographically."
         listTegnologys={['HTML', 'CSS', 'Javascript']}
-        srcLink="https://media.githubusercontent.com/media/DelfosseDoran/portfolio/main/portfolio/src/assets/photos/project/Schermafbeelding%202023-04-01%20232926.jpg"
+        srcLink="projects/782shots_so_metlyd.png"
         link="https://delfossedoran.github.io/eindopdracht-Interaction/"
         start="col-start-2"
         reverse="flex-row-reverse"
+        alt="foto of europe population"
       />
       <Project
         projectName="Project One"
         projectText="The first Project I made. It was the first project I made it was a car monitor. you could see consumption, speed and distance."
         listTegnologys={['raspberrypi', 'Python', 'HTML', 'CSS', 'Javascript']}
-        srcLink="https://media.githubusercontent.com/media/DelfosseDoran/portfolio/main/portfolio/src/assets/photos/project/20220614_113446.jpg"
+        srcLink="projects/20220614_113446_hpiuhn.jpg"
         link="https://www.instructables.com/Project-One/"
         start="col-start-1"
+        alt="foto of project one"
       />
     </main>
   );
